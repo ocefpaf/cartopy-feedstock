2f2cc5f65304db01f6d86167ad983bef661a26f9
Filipe Fernandes <ocefpaf@gmail.com>
2019-07-12 13:00:04 -0500

    backport #1289

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,8 @@ set LIB=%LIBRARY_LIB%;%LIB%
 set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 
+set CYTHON_COVERAGE=1
+
 rmdir lib\cartopy\tests\mpl\baseline_images /s /q
 
 %PYTHON% -m pip install --no-deps --ignore-installed .
